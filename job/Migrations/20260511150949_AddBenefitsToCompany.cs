7967224b8ac07d0d39a0aaae82d5cdd4777e3886
Thai-DM <duongminhthaia1anh@gmail.com>
2026-05-11 22:10:21 +0700

    newfeat

---
+++ job/Migrations/20260511150949_AddBenefitsToCompany.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace job.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBenefitsToCompany : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Benefits",
+                table: "Companies",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Benefits",
+                table: "Companies");
+        }
+    }
+}
--- job/obj/Debug/net8.0/job.AssemblyInfo.cs
+++ job/obj/Debug/net8.0/job.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("job")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86ec6dd3e650702a10b24bf53ee568c4ee288128")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+398184770e1eb7607fc0dfb3b933040db0824d2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("job")]
 [assembly: System.Reflection.AssemblyTitleAttribute("job")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
